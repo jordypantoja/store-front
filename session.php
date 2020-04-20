e8bf824af2a7136f0802272539c76e965081c357
Jordy Pantoja <49248410+jordypantoja@users.noreply.github.com>
2020-04-20 11:46:55 -0400

    Update session.php

--- session.php
+++ session.php
@@ -1,9 +1,11 @@
 <?php
+// Ensures we have started the session to keep track of backend logins
 session_name('store');
 if(!isset($_SESSION)) 
 { 
 	session_start(); 
 } 
+// Loads DB Connection
 include('connect.php');
 
 ?>
